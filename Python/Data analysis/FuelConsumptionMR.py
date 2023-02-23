aa701a5c6946dfc45c23bbf050e4aac31168cb67
Josh Vargas N77~ <joshvargasm3@hotmail.com>
2023-02-23 16:15:36 -0600

    Correccion en linea 4

--- Python/Data analysis/FuelConsumptionMR.py
+++ Python/Data analysis/FuelConsumptionMR.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import pylab as pl
-import numpy as np
-% matplotlib inline
+import numpy as np 
+
 
 url="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%202/data/FuelConsumptionCo2.csv"
 
@@ -41,6 +41,6 @@ y_hat= regr.predict(test[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB']])
 x = np.asanyarray(test[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB']])
 y = np.asanyarray(test[['CO2EMISSIONS']])
 print("Residual sum of squares: %.2f"
-      % np.mean((y_hat - y) ** 2))
+    % np.mean((y_hat - y) ** 2))
 # Explained variance score: 1 is perfect prediction
 print('Variance score: %.2f' % regr.score(x, y))
