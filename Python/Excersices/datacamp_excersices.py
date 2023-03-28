8ec1c398f7660a6480d26fc97ef94cb8971f6cd9
Josh Vargas N77~ <joshvargasm3@hotmail.com>
2023-03-28 11:52:08 -0600

    Certificates DataCamp

---
+++ Python/Excersices/datacamp_excersices.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+x = np.array([9, 5])
+y = np.array([16, 12])
+
+print(np.logical_or(x < 5, y > 15))
+
+datacamp = { 'course':'python', 'level':'intermediate', 'lesson': {'dictionaries':'python', 'lists':'r' } }
+
+datacamp.keys()
